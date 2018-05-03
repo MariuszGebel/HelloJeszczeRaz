b858c1484422cd875f0faeadc4f29b03959c6608
MariuszGebel <gebel@wp.pl>
2018-05-03 20:52:45 +0200

    Second Commit MG

--- src/com/kodilla/Main.java
+++ src/com/kodilla/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello jeszcze raz");
-
+        System.out.println("Hello i jeszcze drugi raz");
 
     }
 }
