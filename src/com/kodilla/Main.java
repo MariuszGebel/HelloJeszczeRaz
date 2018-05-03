0ec0b08606d1bb57a143c2e8f36f492b6c9a3b86
MariuszGebel <gebel@wp.pl>
2018-05-03 20:59:19 +0200

    Third commit MG

--- src/com/kodilla/Main.java
+++ src/com/kodilla/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello jeszcze raz");
         System.out.println("Hello i jeszcze drugi raz");
+        System.out.println("Hello po raz trzeci");
+
 
     }
 }
